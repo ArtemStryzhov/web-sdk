8dbfb4c199b17da27a6ab3a2d663ec4e97cc910f
ChernenkoEugene <_email@.com>
2025-09-01 20:11:02 +0200

    cyberpank: assets updates

--- packages/components-shared/src/components/LoaderExample.svelte
+++ packages/components-shared/src/components/LoaderExample.svelte
@@ -7,28 +7,12 @@
 
 	const props: Props = $props();
 
-	let showText = $state(true);
 </script>
 
 <LoaderBase
 	maxWidth={200}
 	backgroundColor={'#000000'}
-	timeout={2000}
+	timeout={5000}
 	src={props.src}
-	oncomplete={() => (showText = false)}
 />
 
-{#if showText}
-	<span class="add-your-loader">Add Your Loader</span>
-{/if}
-
-<style lang="scss">
-	.add-your-loader {
-		z-index: 999;
-		color: white;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, 10px);
-	}
-</style>
